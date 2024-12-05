13bee5d494cac579a070e202427b8fe9c9ec7b58
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-12-05 21:17:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-//your JS code here. If required.
-const currentUrl = window.location.href;
-
-// const urlLength = currentUrl.length;
-
-// alert(`The length of the URL is: ${urlLength}`);
-
-const url = new URL(currentUrl);
-
-// Get the hostname (domain) only
-const domain = url.hostname;
-
-// Get the length of the domain
-const domainLength = domain.length;
-
-// Display the result
-alert(`The length of the domain is: ${domainLength}`);
+const currentUrl[] = window.location.href;
+const urlLength = currentUrl.length();
+alert(`The length of the URL is: ${urlLength}`);
